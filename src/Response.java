dcf22c27beec1c597aa658de4f469592d2eff552
inngutorova <81492356+inngutorova@users.noreply.github.com>
2021-04-24 02:44:45 +0300

    Update Response.java

--- src/Response.java
+++ src/Response.java
@@ -9,6 +9,7 @@ public class Response {
     public Response(){
     }
 
+    //метод, который позволет вписать текст в определенную область, то есть переносит слова на следующую строчку, когда они не помещаются
     public void writeInRect(Graphics g) {
         int x =0;
         int width = 720;
